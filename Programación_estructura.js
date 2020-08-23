bbe43d53707ba265761ad529e92b943c7f917347
Alisvas21 <avasquezm@josegalan.edu.co>
2020-08-23 17:15:14 -0500

    Programción POO

---
+++ Programación_estructura.js
@@ -0,0 +1,18 @@
+//definir variable 
+
+let nombre = 'Alisson';
+let apellido = 'Vásquez';
+let edad = 23; 
+
+console.log (`Hola mi nombre es: ${nombre} ${apellido} y tengo ${edad} años`);
+
+//función
+
+let num = 20;
+
+function multiplicar(valor) {
+    return valor*valor;
+}
+
+let resultado= multiplicar(5)*num;
+console.log(resultado)
